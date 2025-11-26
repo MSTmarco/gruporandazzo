05ca40c67f768fbefe2d42c23f5b2c7ed60e471d
marcomanuelmz <68705162+marcomanuelmz@users.noreply.github.com>
2025-11-26 11:10:41 +0000

    v2.2

--- src/models/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Models Index
- * Export all models from a single entry point
- */
-
-export { Vehicle } from './Vehicle.js';
-export { Reservation } from './Reservation.js';
-export { User } from './User.js';
-export { Alert } from './Alert.js';
